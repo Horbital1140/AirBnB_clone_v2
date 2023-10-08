39b2ebbc46bf50067476f869f1a11b6c2bdcac2d
horbital1140 <adeemma1993@gmail.com>
2023-10-08 18:15:50 +0000

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,16 @@
-#!/usr/bin/python3
-"""Fabric script that compresses my static files with gzip """
-
 from fabric.api import task, local
 from datetime import datetime as stamp
 
 
 @task(alias="pack")
 def do_pack():
-	"""this function returns gzip compresed file"""
-	try:
-		time_stamp = stamp.now().strftime('%Y%m%d%H%M%S')
-		path = f"versions/web_static_{time_stamp}.tgz"
-		local("mkdir -p versions")
-		local(f"tar -cvzf {path} web_static")
-		return path
-	except Exception:
-		return None
+    """Function that returns a gzip compressed file"""
+    try:
+        time_stamp = stamp.now().strftime('%Y%m%d%H%M%S')
+        path = f"versions/webstatic{time_stamp}.tgz"
+        local("mkdir -p versions")
+        local(f"tar -cvzf {path} web_static")
+    except Exception:
+        return None
+    else:
+        return path
