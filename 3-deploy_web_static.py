683ab236bad559a77c370598d8838ee2cfc8fb76
horbital1140 <adeemma1993@gmail.com>
2023-10-08 20:37:36 +0000

    web deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,68 +1,48 @@
 #!/usr/bin/python3
-""" This module is used to deploy static files to cloud """
-from fabric.api import task, local, run, env, put, hosts, runs_once
-from datetime import datetime as stamp
-from os import path
-from hosts import *
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
 
-@task
-@runs_once
-def do_pack():
-    """Function that returns a gzip compressed file"""
-    try:
-        time_stamp = stamp.now().strftime("%Y%m%d%H%M%S")
-        __path = f"versions/web_static_{time_stamp}.tgz"
-        local("mkdir -p versions")
-        local(f"tar -cvzf {__path} web_static")
-    except Exception:
-        return None
-    else:
-        return __path
 
-
-@task
 def do_deploy(archive_path):
-    """Function distributes an archive to my web servers"""
-    if not path.exists(archive_path):
-        return False
+    """Distributes an archive to a web server.
 
-    releases = "/data/web_static/releases"
-    if put(archive_path, "/tmp/").failed:
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    web_static = path.basename(archive_path).split(".")[0]
-    mkdir_command = "mkdir -p {}/{}".format(releases, web_static)
-    if run(mkdir_command).failed:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-    tar_command = "tar -xzf /tmp/{}.tgz -C {}/{} --strip-components=1".format(
-        web_static, releases, web_static
-    )
-    if run(tar_command).failed:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-
-    rm_archive_command = "rm -f /tmp/{}.tgz".format(web_static)
-    if run(rm_archive_command).failed:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-
-    rm_current_command = "rm -rf /data/web_static/current"
-    if run(rm_current_command).failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
-
-    ln_command = "ln -sf {}/{} /data/web_static/current".format(releases,
-                                                                web_static)
-    if run(ln_command).failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
     return True
-
-@task
-def deploy():
-    """Full deployment to cloud"""
-    archive = do_pack()
-
-    if not archive:
-        return False
-
-    return do_deploy(archive)
