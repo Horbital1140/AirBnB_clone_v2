26703fe5ad9cd3f6b2e052c6dfcbd00ee1be1d9c
Horbital1140 <adeemma1993@gmail.com>
2023-10-09 19:48:33 +0000

    deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-from fabric.api import run, local, task, lcd, cd, put, env
+from fabric.api import run, local, task, lcd, cd, put, runs_once
 from os import listdir, path
 from datetime import datetime as stamp
-from hosts import *
 
 
 env.hosts = ["100.25.188.129", "204.236.240.90"]
 
 
+@runs_once
+@task(alias="pack")
 def do_pack():
     """Function that returns a gzip compressed file"""
     try:
-        time_stamp = stamp.now().strftime("%Y%m%d%H%M%S")
-        __path = f"versions/web_static_{time_stamp}.tgz"
+        time_stamp = stamp.now().strftime('%Y%m%d%H%M%S')
+        __path = "versions/web_static_{}.tgz".format(time_stamp)
         local("mkdir -p versions")
-        local(f"tar -cvzf {__path} web_static")
+        local("tar -cvzf {} web_static".format(__path))
     except Exception:
         return None
     else:
         return __path
 
 
+@task
 def do_deploy(archive_path):
     """Function distributes an archive to my web servers"""
     try:
@@ -32,11 +34,14 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         run("rm -rf {}/{}/".format(releases, web_static))
         run("mkdir -p {}/{}/".format(releases, web_static))
-        run("tar -xzf /tmp/{}.tgz -C {}/{}".format(web_static, releases,
+        run("tar -xzf /tmp/{}.tgz -C {}/{}".format(web_static,
+                                                   releases,
                                                    web_static))
         run("rm /tmp/{}.tgz".format(web_static))
-        run("mv {0}/{1}/web_static/* {0}/{1}/".format(releases, web_static))
-        run("rm -rf {}/{}/web_static".format(releases, web_static))
+        run("mv {0}/{1}/web_static/* {0}/{1}/".format(releases,
+                                                      web_static))
+        run("rm -rf {}/{}/web_static".format(releases,
+                                             web_static))
         run("rm -rf /data/web_static/current")
         run("ln -s {}/{}/ /data/web_static/current".format(releases,
                                                            web_static))
