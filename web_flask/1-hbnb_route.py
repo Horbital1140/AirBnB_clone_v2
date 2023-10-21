a11ee64a3fcdb6d65b830085ddfe33cb3fb79f9b
Horbital1140 <adeemma1993@gmail.com>
2023-10-21 07:38:22 +0100

    web flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this script Starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000.
+Routes:
+        /: it Displays 'Hello HBNB!'
+        """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def horbit():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def horbit_2():
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
