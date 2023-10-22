70bc2852ed8efd9a197ed846075c472cf7c0ef93
Horbital1140 <adeemma1993@gmail.com>
2023-10-22 12:57:38 +0100

    web flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,45 @@
-"""this script Starts a Flask web application.                                                                                                                                  The web application listens on 0.0.0.0, port 5000.                                      Routes:                                                                                                 /: it Displays 'Hello HBNB!'                                                                                """                                                 from flask import Flask, render_template                                                                 app = Flask(__name__)                                                                                                                                                                                                                                                   @app.route('/', strict_slashes=False)                                                   def horbit():                                                                               return 'Hello HBNB!'                                                                                                                                                                                                                                                @app.route('/hbnb', strict_slashes=False)                                               def horbit_2():                                                                             return 'HBNB'                                                                                                                                                                                                                                                       @app.route('/c/<text>', strict_slashes=False)                                           def horbit_text(text):                                                                      text = text.replace("_", " ")                                                           return f"C {text}"                                                                                                                                                                                                                                                  @app.route('/python/<text>', strict_slashes=False)                                      def horbit_python(text="is cool"):                                                          text = text.replace("_", " ")                                                           return f"python {text}"                                                                                                                                                                                                                                             @app.route('/number/<int:n>', strict_slashes=False)                                     def horbit_integer(n):                                                                      return f"{n} is a number"                                                                                                                                                   
+#!/usr/bin/python3
+"""this script Starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000.
+Routes:
+                    /: it Displays 'Hello HBNB!'
+                                                        """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def horbit():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def horbit_2():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def horbit_text(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def horbit_python(text="is cool"):
+    text = text.replace("_", " ")
+    return f"python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def horbit_integer(n):
+    return f"{n} is a number"
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def horbit_num_template(n):
-    return render_template('5-number.html", number=n')
+def horbit_number_template(n):
+    return render_template("5-number.html", number=n)
+
 
 if __name__ == "__main__":
-    APP.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)      
